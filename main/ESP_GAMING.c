aa27f785dbdd2315cad6a6cd569814f4950b9ecc
Ewerton Lopes <ewerton.va.lopes@gmail.com>
2024-04-27 09:09:02 -0300

    Working base

--- main/ESP_GAMING.c
+++ main/ESP_GAMING.c
@@ -1,6 +1,112 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
 
-void app_main(void)
-{
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
 
+#include "axp192.h"
+#include "st7735s.h"
+#include "fontx.h"
+#include "bmpfile.h"
+#include "decode_jpeg.h"
+#include "decode_png.h"
+#include "pngle.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 160
+#define OFFSET_X 0
+#define OFFSET_Y 0
+#define GPIO_MOSI 22
+#define GPIO_SCLK 23
+#define GPIO_CS 18
+#define GPIO_DC 19
+#define GPIO_RESET 21
+
+static const char *TAG = "ESP_GAMING";
+
+static void SPIFFS_Directory(char * path) {
+	DIR* dir = opendir(path);
+	assert(dir != NULL);
+	while (true) {
+		struct dirent*pe = readdir(dir);
+		if (!pe) break;
+		ESP_LOGI(TAG,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
+	}
+	closedir(dir);
+}
+
+void app_main(void) {
+
+	ESP_LOGI(TAG, "Initializing SPIFFS");
+
+	esp_vfs_spiffs_conf_t conf = {
+		.base_path = "/spiffs",
+		.partition_label = NULL,
+		.max_files = 10,
+		.format_if_mount_failed =true
+	};
+
+	// Use settings defined above toinitialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is anall-in-one convenience function.
+	esp_err_t ret =esp_vfs_spiffs_register(&conf);
+
+	if (ret != ESP_OK) {
+		if (ret ==ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount or format filesystem");
+		} else if (ret== ESP_ERR_NOT_FOUND) {
+			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
+		}
+		return;
+	}
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(NULL, &total,&used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG,"Failed to get SPIFFS partition information (%s)",esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
+	}
+
+	SPIFFS_Directory("/spiffs");
+
+	// set font file
+	FontxFile fx16[2];
+	FontxFile fx24[2];
+	FontxFile fx32[2];
+	InitFontx(fx16,"/spiffs/ILGH16XB.FNT",""); // 8x16Dot Gothic
+	InitFontx(fx24,"/spiffs/ILGH24XB.FNT",""); // 12x24Dot Gothic
+	InitFontx(fx32,"/spiffs/ILGH32XB.FNT",""); // 16x32Dot Gothic
+
+	//InitFontx(fx16,"/spiffs/ILMH16XB.FNT",""); // 8x16Dot Mincyo
+	//InitFontx(fx24,"/spiffs/ILMH24XB.FNT",""); // 12x24Dot Mincyo
+	//InitFontx(fx32,"/spiffs/ILMH32XB.FNT",""); // 16x32Dot Mincyo
+
+	ST7735_t dev;
+	spi_master_init(&dev, GPIO_MOSI, GPIO_SCLK, GPIO_CS, GPIO_DC, GPIO_RESET);
+	lcdInit(&dev, SCREEN_WIDTH, SCREEN_HEIGHT, OFFSET_X, OFFSET_Y);
+
+
+    while (1){
+        lcdDrawFillRect(&dev, 20, 0, 40, 5, BLACK);
+        lcdDrawFillRect(&dev, 20, 0, 40, 5, WHITE);
+
+        lcdDrawFillCircle(&dev, 60, 60, 3, BLACK);
+        lcdDrawFillCircle(&dev, 60, 60, 3, WHITE);
+
+        lcdDrawFillRect(&dev, 20, 155, 40, 160, BLACK);
+        lcdDrawFillRect(&dev, 20, 155, 40, 160, WHITE);
+
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
 }
